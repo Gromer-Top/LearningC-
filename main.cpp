c50e1103b7644df763a302e95592e4bf9129d25a
Gromer <rus.solovey.09@mail.ru>
2025-02-23 18:14:16 +0300

    new file:   .vscode/launch.json 	modified:   .vscode/settings.json 	modified:   main.cpp 	modified:   main.exe

--- main.cpp
+++ main.cpp
@@ -1,18 +1,62 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <windows.h>
 
-class Car
-{
-    public:
-        int year;
-
-        Car(int year)
-        {
-            this->year = year;
-        }
-};
+int randomizeGuess();
+int enterUserGuess(std::vector <std::string> variants);
+void calcResultGame(int userGuess, int compGuess, std::vector <std::string> variants);
 
 int main()
 {
-    Car haval(2023);
-    std::cout << haval.year;
+    //кодировку на кириллицу переключаем
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    //----------------------------------
+    int userGuess(0), compGuess(0);
+    std::vector <std::string> variants {"Ножницы", "Камень", "Бумага"};
+    srand(time(0));
+    compGuess = randomizeGuess();
+    userGuess = enterUserGuess(variants);
+    calcResultGame(userGuess, compGuess, variants);
+}
+
+int randomizeGuess()
+{
+    int min = 0;
+    int max = 2;
+    int res = rand() % (max - min + 1) + min;
+    return res;
+}
+
+int enterUserGuess(std::vector <std::string> variants)
+{
+    int userGuess;
+    std::cout << "Варианты: \n";
+    for (int i = 0; i < size(variants); i++)
+    {
+        std::cout << i + 1 << ": " << variants[i] << "\n";
+    }
+    std::cout << "Введите свой выбор: ";
+    std::cin >> userGuess;
+    return userGuess - 1;
+}
+
+void calcResultGame(int userGuess, int compGuess, std::vector <std::string> variants)
+{
+    if (userGuess == 0 && compGuess == 2 || userGuess == 1 && compGuess == 0 || userGuess == 2 && compGuess == 1)
+    {
+        std::cout << "Ты выбрал: " << variants[userGuess] << "\t" << "Компьютер выбрал: " << variants[compGuess] << "\n";
+        std::cout << "Ты выйграл!";
+    }
+    else if (userGuess == 0 && compGuess == 0 || userGuess == 1 && compGuess == 1 || userGuess == 2 && compGuess == 2)
+    {
+        std::cout << "Ты выбрал: " << variants[userGuess] << "\t" << "Компьютер выбрал: " << variants[compGuess] << "\n";
+        std::cout << "Ничья!";
+    }
+    else
+    {
+        std::cout << "Ты выбрал: " << variants[userGuess] << "\t" << "Компьютер выбрал: " << variants[compGuess] << "\n";
+        std::cout << "Ты проиграл!";
+    }
 }
